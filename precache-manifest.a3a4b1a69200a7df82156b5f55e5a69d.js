a1d03b49225491ef98b80a8d87ad6c369b3977a9
mudassar045 <mudassaralics007@gmail.com>
2020-10-18 20:36:16 +0500

    Updates

---
+++ precache-manifest.a3a4b1a69200a7df82156b5f55e5a69d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a5b729baf86a90baa8539c50516683",
+    "url": "/starmap-web/index.html"
+  },
+  {
+    "revision": "4cbcf74800671cf72fd0",
+    "url": "/starmap-web/static/css/main.a414de92.chunk.css"
+  },
+  {
+    "revision": "929d11e8f4bd6dc13127",
+    "url": "/starmap-web/static/js/2.d09dcff9.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/starmap-web/static/js/2.d09dcff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cbcf74800671cf72fd0",
+    "url": "/starmap-web/static/js/main.5df1f447.chunk.js"
+  },
+  {
+    "revision": "23914998c3eb97c2a253",
+    "url": "/starmap-web/static/js/runtime-main.09bceb1c.js"
+  },
+  {
+    "revision": "f07e5eaba43268cb5a4d40eb18d84b7c",
+    "url": "/starmap-web/static/media/machine-learning.f07e5eab.svg"
+  }
+]);
